a67bf3a0b037ecdf24a488ec5d9b7f9b4f56c527
murudvishwa-gif <murudvishwa@gmail.com>
2026-06-12 14:07:23 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+  document.getElementById("mobileMenu").classList.add("open");
+}
+function closeMenu(){
+  document.getElementById("mobileMenu").classList.remove("open");
+}
